960b0c9d1afa854e013c1658f5c9ffd8eae198d5
Jeremiah Dost <jeremy.dost@gmail.com>
2020-05-15 08:02:05 -0700

    E22 - Main Menu implementation

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	public string levelToLoad = "MainLevel";
+
+	public void Play()
+	{
+		Debug.Log($"Opening level {levelToLoad}");
+		SceneManager.LoadScene(levelToLoad);
+	}
+
+	public void Quit()
+	{
+		Debug.Log("Exiting");
+		Application.Quit();
+	}
+}
